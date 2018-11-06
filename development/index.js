e58dde708c4b95a10426dd9b48312caec18fb9bd
ankuda <qwinsayd@mail.ru>
2018-11-06 15:28:31 +0300

    formatted graph, demoTable and coresponding css

--- development/index.js
+++ development/index.js
@@ -1,29 +1,29 @@
-import { TableDemo }                from './components/tableDemo';
-import { TransportationData }       from './components/transportationData';
-import { RealTimeInformation }      from './components/realTimeInformation';
-import { GraphContent }             from './components/graphContent';
-import { CaseStudies }              from './components/caseStudies';
-import { BenefitsContent }          from './components/benefitsContent';
-import { TypesTransportationData }  from './components/typesTransportationData';
-import { Map }                      from './components/map';
-import { Standarts }                from './components/standarts';
-import { Technologies }             from './components/technologies';
-import { Footer }                   from "./components/footer";
+import { TableDemo } from "./components/tableDemo";
+import { TransportationData } from "./components/transportationData";
+import { RealTimeInformation } from "./components/realTimeInformation";
+import { GraphContent } from "./components/graphContent";
+import { CaseStudies } from "./components/caseStudies";
+import { BenefitsContent } from "./components/benefitsContent";
+import { TypesTransportationData } from "./components/typesTransportationData";
+import { Map } from "./components/map";
+import { Standarts } from "./components/standarts";
+import { Technologies } from "./components/technologies";
+import { Footer } from "./components/footer";
 
-import { styles }                   from "./scss/styles.scss";
+import { styles } from "./scss/styles.scss";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 const inn = root.innerHTML;
 root.innerHTML =
-                  // TableDemo               +
-                  TransportationData      +
-                  RealTimeInformation     +
-                  GraphContent            +
-                  CaseStudies             +
-                  BenefitsContent         +
-                  TypesTransportationData +
-                  Map                     +
-                  Standarts               +
-                  Technologies            +
-                  Footer                  +
-                  inn;
+  // TableDemo               +
+  TransportationData +
+  RealTimeInformation +
+  GraphContent +
+  CaseStudies +
+  BenefitsContent +
+  TypesTransportationData +
+  Map +
+  Standarts +
+  Technologies +
+  Footer +
+  inn;
